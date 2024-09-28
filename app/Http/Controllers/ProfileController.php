cb3cd575deaff6158a2f001b4bd929aa1e2bed54
unknown <azadgmteam@gmail.com>
2024-09-28 17:00:16 +0600

    assignment-three

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $value = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        return response()->json($data, 200)
+            ->cookie($cookieName, $value, $minutes, $path, $domain, $secure, $httpOnly);
+    }
+}
